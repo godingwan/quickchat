85a3eef43754dc078bab15faec6a32850c753f33
Daniel Lawrence <64.74.6c@gmail.com>
2016-11-07 14:32:40 -0500

    Actually does something now!

--- main.go
+++ main.go
@@ -8,48 +8,72 @@ import (
 
 var cns []net.Conn
 
+var msg chan string
+
+type client struct {
+	conn     *net.Conn
+	username string
+}
+
 func main() {
 	fmt.Println("Starting quickchat...")
+	msg = make(chan string)
 	netInit(10)
-	time.Sleep(10 * time.Second)
+	go func() {
+		for m := range msg {
+			fmt.Println(m)
+		}
+	}()
+	for {
+		time.Sleep(3 * time.Second)
+		// send test string to all connected clients
+		for c := range cns {
+			fmt.Fprint(cns[c], "Hello fellow client!\n")
+		}
+	}
 }
 
 func netInit(connectionLimit int) {
 	cns = make([]net.Conn, 0, connectionLimit)
 	// start broadcasting for availability using Dial()
-	go requestConnections(2000)
+	requestConnections()
 	// start listening for requests using Listen()
 	go acceptConnections()
 	// start listening for responses using Listen()
 	go handshake()
 }
 
-// repeatedly broadcast availability on port 875
-func requestConnections(timeout int) {
-	for {
-		time.Sleep(time.Duration(timeout) * time.Millisecond)
-		conn, err := net.Dial("udp", "255.255.255.255:875")
-		if err != nil {
-			fmt.Printf("Some error: %v\n", err)
-			continue
-		}
-		fmt.Fprintf(conn, "request tcp")
+// broadcast availability on port 875
+func requestConnections() {
+	fmt.Println("Sending UDP broadcast")
+	conn, err := net.Dial("udp", "255.255.255.255:875")
+	if err != nil {
+		fmt.Printf("Broadcast error: %v\n", err)
+		return
 	}
+	conn.Write([]byte("request tcp"))
+	conn.Close()
 }
 
 // listen for tcp responses on 876
 func acceptConnections() {
-	ln, err := net.Listen("tcp", ":876")
+	laddr := net.TCPAddr{
+		IP:   nil,
+		Port: 876,
+	}
+	ln, err := net.ListenTCP("tcp", &laddr)
 	if err != nil {
 		fmt.Printf("Can't accept connections! Err: %v\n", err)
 	}
 	for {
-		conn, err := ln.Accept()
+		conn, err := ln.AcceptTCP()
+		fmt.Println("Accepted tcp connection.")
 		if err != nil {
 			fmt.Printf("Error accepting connections: %v\n", err)
 		} else {
 			fmt.Printf("Accepted tcp connection from %s\n", conn.RemoteAddr())
 			cns = append(cns, conn)
+			readIncomingMessages(conn)
 		}
 	}
 }
@@ -63,14 +87,44 @@ func handshake() {
 		Port: 875,
 		IP:   net.ParseIP("255.255.255.255"),
 	}
+	fmt.Println("Listening for UDP...")
 	ser, err := net.ListenUDP("udp", &udpAddr)
 	if err != nil {
 		fmt.Printf("Error responding to UDP broadcast: %v\n", err)
 	}
 	for {
+		fmt.Println("Accepting UDP...")
 		_, remoteAddr, err := ser.ReadFromUDP(buff)
 		if err == nil {
 			fmt.Printf("Recieved msg '%s'\nfrom addr: %v\n", buff, remoteAddr)
+			go makeConnection(remoteAddr)
 		}
+	}
+}
+
+func makeConnection(remoteAddr *net.UDPAddr) {
+	toDial := net.TCPAddr{
+		Port: 876,
+		IP:   remoteAddr.IP,
+	}
+	fmt.Printf("Making connection with %s\n", toDial.IP.String())
+	conn, err := net.DialTCP("tcp", nil, &toDial)
+	if err == nil {
+		fmt.Printf("Successful connection with %s\n", remoteAddr.String())
+		cns = append(cns, conn)
+		readIncomingMessages(conn)
+	}
+}
+
+func readIncomingMessages(c *net.TCPConn) {
+	buff := make([]byte, 2048)
+	for {
+		n, err := c.Read(buff)
+		if err != nil {
+			fmt.Printf("TCP read error, closing connection: %v\n", err)
+			c.Close()
+			return
+		}
+		msg <- string(buff[:n])
 	}
 }
